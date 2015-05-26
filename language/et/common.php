bb6c2533b72ec03157d34016cfa1f0b1c3d10f2b
phpBBeesti <info@phpbbeesti.com>
2015-05-26 13:53:49 +0300

    Update common.php

--- language/et/common.php
+++ language/et/common.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package phpBB Extension - tas2580 Social Media Buttons
-* @copyright (c) 2014 tas2580 (https://tas2580.net) ; Estonian translation by http://www.phpbbeesti.com/
+* @copyright (c) 2014 tas2580 (https://tas2580.net) ; Estonian translation by http://www.phpbbeesti.com/ and jFlash (eraser.ee)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
