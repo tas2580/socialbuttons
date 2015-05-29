9e1758e110d078711fa15c3f105d4a48a4e37e13
Raphaël M. <Galixte@users.noreply.github.com>
2015-05-29 15:46:54 +0200

    Update for phpBB 3.1.4
    update for common.php file.

--- language/fr/common.php
+++ language/fr/common.php
@@ -8,15 +8,34 @@
 *
 */
 
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
-    exit;
+	exit;
 }
 
 if (empty($lang) || !is_array($lang))
 {
-    $lang = array();
+	$lang = array();
 }
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
 
 $lang = array_merge($lang, array(
     'SOCIAL_BUTTONS'			=> 'Réseau social',
@@ -64,4 +83,6 @@ $lang = array_merge($lang, array(
 	'ACP_USE_SEO_URLS_EXPLAIN'	=> 'Activer cette option si une extension concernant l’URL Rewriting est activée.',
 	'ACP_ENABLE_FORUMS'			=> 'Activer les boutons',
 	'ACP_ENABLE_FORUMS_EXPLAIN'	=> 'Sélectionner les forums où les boutons doivent être affichés.',
+	'ACP_DISPLAY_ON_INDEX'	=> 'Afficher sur l’index',
+	'ACP_DISPLAY_ON_INDEX_EXPLAIN'	=> 'Affiche sur la page de l’index du forum les boutons des réseaux sociaux au-dessus du « Qui est en ligne ? ».',
 ));
