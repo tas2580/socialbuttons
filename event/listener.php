f2262f1efcc886825436e37e3b96d4c8029c02d3
tas2580 <tobi.schaefer@gmail.com>
2015-06-15 17:12:09 +0200

    Fixed #14

--- event/listener.php
+++ event/listener.php
@@ -4,7 +4,7 @@
 * @package phpBB Extension - tas2580 Social Media Buttons
 * @copyright (c) 2015 tas2580 (https://tas2580.net)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* 
+*
 */
 
 namespace tas2580\socialbuttons\event;
@@ -24,13 +24,13 @@ class listener implements EventSubscriberInterface
 
 	/** @var \phpbb\user\user */
 	protected $user;
-	
+
 	/* @var \phpbb\request\request */
 	protected $request;
-	
+
 	/** @var string phpbb_root_path */
 	protected $phpbb_root_path;
-	
+
 	/**
 	* Constructor
 	*
@@ -66,7 +66,7 @@ class listener implements EventSubscriberInterface
 			'core.index_modify_page_title'			=> 'display_on_index'
 		);
 	}
-	
+
 	/**
 	* Display the first post as Open Graph description
 	*
@@ -86,7 +86,7 @@ class listener implements EventSubscriberInterface
 			));
 		}
 	}
-	
+
 	/**
 	* Display Social Media Buttons at the index page
 	*
@@ -101,14 +101,14 @@ class listener implements EventSubscriberInterface
 		// Display the shares count
 		if($enable_buttons && isset($this->config['socialbuttons_showshares']) && $this->config['socialbuttons_showshares'])
 		{
-			$shares = $this->get_share_count($url);
+			$shares = $this->get_share_count($url, 'index');
 			$this->template->assign_vars(array(
 				'S_SHOWSHARES'			=> true,
 				'SHARES_FACEBOOK'		=> isset($shares['facebook']) ? (int) $shares['facebook'] : 0,
 				'SHARES_TWITTER'			=> isset($shares['twitter']) ? (int) $shares['twitter'] : 0,
 				'SHARES_GOOGLE'			=> isset($shares['google']) ? (int) $shares['google'] : 0,
 				'SHARES_LINKEDIN'			=> isset($shares['linkedin']) ? (int) $shares['linkedin'] : 0,
-			));	
+			));
 		}
 
 		// Display the buttons and the OG meta tags
@@ -126,9 +126,9 @@ class listener implements EventSubscriberInterface
 			'OG_URL'					=> $url,
 			'OG_TITLE'				=> $event['page_title'],
 		));
-		$this->user->add_lang_ext('tas2580/socialbuttons', 'common');					
+		$this->user->add_lang_ext('tas2580/socialbuttons', 'common');
 	}
-	
+
 	/**
 	* Display Social Media Buttons at topics page
 	*
@@ -140,7 +140,7 @@ class listener implements EventSubscriberInterface
 	{
 		$enabled_forums = isset($this->config['socialbuttons_enable_forums']) ? explode(',', $this->config['socialbuttons_enable_forums']) : array();
 		$enable_buttons = ((isset($this->config['socialbuttons_enable']) && $this->config['socialbuttons_enable']) || in_array($event['topic_data']['forum_id'], $enabled_forums));
-		
+
 		// Generate the full URL of the topic without session ID
 		$use_seo_urls = isset($this->config['socialbuttons_use_seo_urls']) ? $this->config['socialbuttons_use_seo_urls'] : 0;
 		if($use_seo_urls)
@@ -160,18 +160,19 @@ class listener implements EventSubscriberInterface
 		{
 			$url = generate_board_url() . '/viewtopic.php?f=' . $event['topic_data']['forum_id'] . '&t=' . $event['topic_data']['topic_id'];
 		}
-		
+
 		// Display the shares count
 		if($enable_buttons && isset($this->config['socialbuttons_showshares']) && $this->config['socialbuttons_showshares'])
 		{
-			$shares = $this->get_share_count($url);
+			$token = 't' . $event['topic_data']['topic_id'] . '-' . $event['start'];
+			$shares = $this->get_share_count($url, $token);
 			$this->template->assign_vars(array(
 				'S_SHOWSHARES'			=> true,
 				'SHARES_FACEBOOK'		=> isset($shares['facebook']) ? (int) $shares['facebook'] : 0,
 				'SHARES_TWITTER'			=> isset($shares['twitter']) ? (int) $shares['twitter'] : 0,
 				'SHARES_GOOGLE'			=> isset($shares['google']) ? (int) $shares['google'] : 0,
 				'SHARES_LINKEDIN'			=> isset($shares['linkedin']) ? (int) $shares['linkedin'] : 0,
-			));	
+			));
 		}
 
 		// Display the buttons and the OG meta tags
@@ -187,7 +188,7 @@ class listener implements EventSubscriberInterface
 			'S_SHOW_AT_TOP'			=> ($position == 0 || $position == 1) ? true : false,
 			'S_SHOW_AT_BOTTOM'		=> ($position == 0 || $position == 2) ? true : false,
 			'U_TOPICLINK'				=> urlencode($url),
-			'OG_URL'					=> $url,			
+			'OG_URL'					=> $url,
 			'ENABLE_OG'				=> isset($this->config['socialbuttons_enable_og']) ? $this->config['socialbuttons_enable_og'] : '',
 			'OG_IMAGE'				=> isset($this->config['socialbuttons_og_image']) ? $this->config['socialbuttons_og_image'] : '',
 			'ENABLE_OG_TITLE'			=> isset($this->config['socialbuttons_enable_og_title']) ? $this->config['socialbuttons_enable_og_title'] : '',
@@ -200,21 +201,22 @@ class listener implements EventSubscriberInterface
 	/**
 	* Get the number of shares
 	*
-	* @param	string	$url	The URL to get the shares for
+	* @param	string	$url		The URL to get the shares for
+	* @param	string	$token	Unique string to identify the page
 	* @return	array
 	* @access private
 	*/
-	private function get_share_count($url)
+	private function get_share_count($url, $token)
 	{
 		$shares = array();
 		$cache_time = isset($this->config['socialbuttons_cachetime']) ? $this->config['socialbuttons_cachetime'] : 0;
 		$multiplicator = isset($this->config['socialbuttons_multiplicator']) ? $this->config['socialbuttons_multiplicator'] : 1;
-		
+
 		$cachetime = ((int) $cache_time * (int) $multiplicator);
-		$cache_file = $this->phpbb_root_path . 'ext/tas2580/socialbuttons/cache/' . md5($url) . '.json';
+		$cache_file = $this->phpbb_root_path . 'ext/tas2580/socialbuttons/cache/' . $token . '.json';
 		$filetime = file_exists($cache_file) ? filemtime($cache_file) : 0;
 		$url = urlencode($url);
-		
+
 		// If cache is too old or we have no cache query the platforms
 		if(($filetime == 0) || ($filetime < (time() - $cachetime)))
 		{
@@ -235,7 +237,7 @@ class listener implements EventSubscriberInterface
 			{
 				$querys['linkedin'] = 'https://www.linkedin.com/countserv/count/share?format=json&url=' . $url;
 			}
-			
+
 			// Do we have curl? We can query all platforms paralel what is mutch faster
 			if(function_exists('curl_multi_init') && function_exists('curl_multi_exec'))
 			{
@@ -247,19 +249,19 @@ class listener implements EventSubscriberInterface
 					curl_setopt($ch, CURLOPT_URL, $query_url);
 					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 					curl_setopt($ch, CURLOPT_NOBODY, false);
-					curl_setopt($ch, CURLOPT_HEADER, false); 
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-					curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0'); 
+					curl_setopt($ch, CURLOPT_HEADER, false);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0');
 					curl_multi_add_handle($mh, $ch);
 					$handle[$platform] = $ch;
 				}
-				
+
 				// Exec the query
 				$running = 0;
-				do 
+				do
 				{
 					curl_multi_exec($mh, $running);
-				} 
+				}
 				while($running > 0);
 
 				// Get the resonse
@@ -282,7 +284,7 @@ class listener implements EventSubscriberInterface
 					$content[$platform] = file_get_contents($query_url, false, $context);
 				}
 			}
-			
+
 			// Get the number of shares from response
 			$matches = array();
 			preg_match('#<div id="aggregateCount" class="Oy">([0-9]+)</div>#s', $content['google'], $matches);
@@ -296,7 +298,7 @@ class listener implements EventSubscriberInterface
 
 			$pageinfo = json_decode($content['linkedin'], true);
 			$shares['linkedin'] = isset($pageinfo['count']) ? $pageinfo['count'] : 0;
-			
+
 			// Write data to cache
 			if(is_writable(dirname($cache_file)))
 			{
