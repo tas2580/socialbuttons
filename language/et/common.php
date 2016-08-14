593b8cf0258949726693647d019495a64f5864fc
phpBBeesti <info@phpbbeesti.net>
2016-08-14 15:05:22 +0300

    Update common.php

--- language/et/common.php
+++ language/et/common.php
@@ -2,59 +2,62 @@
 /**
 *
 * @package phpBB Extension - tas2580 Social Media Buttons
-* @copyright (c) 2014 tas2580 (https://tas2580.net) ; Estonian translation by http://www.phpbbeesti.com/
+* @copyright (c) 2014 tas2580 (https://tas2580.net) ; Estonian translation by http://www.phpbbeesti.com/ and jFlash (eraser.ee)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
-
 if (!defined('IN_PHPBB'))
 {
-	exit;
+    exit;
 }
-
 if (empty($lang) || !is_array($lang))
 {
-	$lang = array();
+    $lang = array();
 }
-// DEVELOPERS PLEASE NOTE
-//
-// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
-//
-// Placeholders can now contain order information, e.g. instead of
-// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
-// translators to re-order the output of data while ensuring it remains correct
-//
-// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
-// equally where a string contains only two placeholders which are used to wrap text
-// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
-//
-// Some characters you may want to copy&paste:
-// ’ » “ ” …
-//
 $lang = array_merge($lang, array(
-	'SOCIAL_BUTTONS'				=> 'Jaga seda sotsiaalmeedias',
-	'SHARE_FACEBOOK'				=> 'Jaga Facebookis',
+    'SOCIAL_BUTTONS'			=> 'Jaga seda sotsiaalvõrgustikus',
+	'SHARE_FACEBOOK'			=> 'Jaga Facebookis',
 	'SHARE_TWITTER'				=> 'Jaga Twitteris',
 	'SHARE_GOOGLEPLUS'			=> 'Jaga Google+\'is',
-	'SHARE_LINKEDIN'				=> 'Jaga Linkedin\'is',
+	'SHARE_LINKEDIN'			=> 'Jaga Linkedin\'is',
 	'ACP_SETTINGS'				=> 'Seaded',
-	'ACP_POSITION'					=> 'Asukoht',
-	'ACP_POSITION_EXPLAIN'			=> 'Asukoht, kus soovid nuppe näidata.',
-	'ACP_SUBMIT'					=> 'Uuenda muudatused',
-	'ACP_POSITION_ALL'				=> 'Ülesse ja alla',
-	'ACP_POSITION_TOP'				=> 'Üles',
-	'ACP_POSITION_BOTTOM'			=> 'Alla',
+	'ACP_POSITION'				=> 'Asukoht',
+	'ACP_POSITION_EXPLAIN'		=> 'Vali asukoht, kus soovid näidata sotsiaalvõrgustikke nuppe.',
+	'ACP_SUBMIT'				=> 'Uuenda muudatused',
+	'ACP_POSITION_ALL'			=> 'Ülesse ja alla',
+	'ACP_POSITION_TOP'			=> 'Üles',
+	'ACP_POSITION_BOTTOM'		=> 'Alla',
 	'ACP_CACHETIME'				=> 'Vahemälu aeg',
-	'ACP_CACHETIME_EXPLAIN'		=> 'Kui kauaks peaks andmed jääma vahemällu?',
+	'ACP_CACHETIME_EXPLAIN'		=> 'Kui kauaks peaks jääma andmed vahemällu?',
 	'ACP_MULTIPLICATOR_SECONDS'	=> 'Sekundit',
 	'ACP_MULTIPLICATOR_MINUTES'	=> 'Minutit',
-	'ACP_MULTIPLICATOR_HOURS'		=> 'Tundi',
+	'ACP_MULTIPLICATOR_HOURS'	=> 'Tundi',
 	'ACP_SET_PLATFORMS'			=> 'Luba järgnevad',
 	'ACP_FACEBOOK'				=> 'Facebook',
-	'ACP_TWITTER'					=> 'Twitter',
-	'ACP_GOOGLE'					=> 'Google Plus',
-	'ACP_LINKEDIN'					=> 'Linkedin',
-	'ACP_SAVED'					=> 'Sotsiaalmeedia nuppude seaded uuendatud.',
+	'ACP_TWITTER'				=> 'Twitter',
+	'ACP_GOOGLE'				=> 'Google Plus',
+	'ACP_LINKEDIN'				=> 'Linkedin',
+	'ACP_SAVED'					=> 'Sotsiaalvõrgustike nupude seaded on uuendatud.',
 	'ACP_STYLE'					=> 'Stiil',
-	'ACP_STYLE_EXPLAIN'			=> 'Vali stiil nupud',
+	'ACP_STYLE_EXPLAIN'			=> 'Vali stiil nuppude jaoks',
+	'CACHE_PATH_NOT_WRITEABLE'	=> 'Vahemälu teekond "%s" ei ole kirjutatav!',
+	'ACP_CACHE_PURGE_SUCCESS'	=> 'Kustutatud kõik failid vahemälus.',
+	'ACP_SHOWSHARES'			=> 'Kuva jagamisi',
+	'ACP_SHOWSHARES_EXPLAIN'	=> 'Kas jagamiste arvu nuppude taga näidatakse või mitte?',
+	'ACP_PURGE_CACHE'			=> 'Puhasta vahemälu',
+	'ACP_PURGE_CACHE_EXPLAIN'	=> 'Kustuta kõik failid vahemälust',
+	'RUN_NOW'					=> 'Käivita nüüd',
+	'ACP_OG_TAGS'				=> 'OG Meta-sildid',
+	'ACP_ENABLE_OG_TAGS'		=> 'Aktiveeri OG Meta-sildid',
+	'ACP_OG_EXPLAIN'			=> 'Aktiveeritakse <a href="http://ogp.me/">Open Graph</a> metavõtmed, et sotsiaalmeedia platvormid oskaksid lehelt võtta jagamiseks infot',
+	'ACP_OG_IMAGE'				=> 'Pilt',
+	'ACP_OG_IMAGE_EXPLAIN'		=> 'Sisesta pildi URL mida tahad jagamise juures kasutada.',
+	'ACP_ENABLE_OG_TITLE'		=> 'Pealkiri',
+	'ACP_OG_TITLE_EXPLAIN'		=> 'Kui on aktiveeritud, siis postituse pealkirja kasutatakse võtmes og:title',
+	'ACP_ENABLE_OG_DESC'		=> 'Kirjeldus',
+	'ACP_OG_DESC_EXPLAIN'		=> 'Kui on aktiveeritud, siis esimest postitust kasutatakse võtmes og:description.<br /><span style="color:#f00;">Tähelepanu! Kui on aktiveeritud, siis esimest postitust näevad kõik, isegi siis kui postitus on privaatses foorumi alas.</span> See on selleks, et iseloomustuse asemel ei näeks teised sisselogimisteksti vaid teemat ennast.',
+	'ACP_USE_SEO_URLS'			=> 'URL ülekirjutamine',
+	'ACP_USE_SEO_URLS_EXPLAIN'	=> 'Aktiveeri see valik, kui URLi ülekirjutamise laiendus on kasutusel.',
+	'ACP_ENABLE_FORUMS'			=> 'Aktiveeri nuppud',
+	'ACP_ENABLE_FORUMS_EXPLAIN'	=> 'Vali foorumid, kus sa soovid näidata sotsiaalvõrgustikke nuppe.',
 ));
